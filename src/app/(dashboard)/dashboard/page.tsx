89341eb48497aef5fd813b7e48e2505b94374ddc
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-08-03 18:05:16 +0400

    Add friend to database works well

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,22 +1,26 @@
-import { Button } from "@/components/ui/button";
-import { authOptions } from "@/lib/auth";
-import { getServerSession } from "next-auth";
+import { Button } from '@/components/ui/button';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import Link from 'next/link';
 
 interface pageProps {}
 
 const page = async ({}) => {
-  const session = await getServerSession(authOptions);
-  return (
-    <div className="p-28 h-screen flex flex-col items-center justify-center gap-2">
-      DAHSBOARD
-      <hr />
-      <Button variant="outline" size="sm">
-        Button
-      </Button>
-      <hr />
-      <pre>{JSON.stringify(session, null, 4)}</pre>
-    </div>
-  );
+    const session = await getServerSession(authOptions);
+
+    return (
+        <div className="p-28 h-screen flex flex-col items-center justify-center gap-2">
+            DAHSBOARD
+            <hr />
+            <Link href="/dashboard/add">
+                <Button variant="outline" size="sm">
+                    Button
+                </Button>
+            </Link>
+            <hr />
+            <pre>{JSON.stringify(session, null, 4)}</pre>
+        </div>
+    );
 };
 
 export default page;
--- src/components/AddFriendButton.tsx
+++ src/components/AddFriendButton.tsx
@@ -1,81 +1,92 @@
-"use client";
+'use client';
 
-import { FC, useState } from "react";
-import { Button } from "./ui/button";
-import { addFriendValidator } from "@/lib/validations/addFriend";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import axios, { AxiosError } from "axios";
-import { zodResolver } from "@hookform/resolvers/zod";
+import { FC, useEffect, useState } from 'react';
+import { Button } from './ui/button';
+import { addFriendValidator } from '@/lib/validations/addFriend';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import axios, { AxiosError } from 'axios';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 interface AddFriendButtonProps {}
 
 type FormData = z.infer<typeof addFriendValidator>;
 
 const AddFriendButton: FC<AddFriendButtonProps> = ({}) => {
-  const [showSuccessState, setShowSuccessState] = useState<boolean>(false);
+    const [showSuccessState, setShowSuccessState] = useState<boolean>(false);
+    const [mounted, setMounted] = useState(false);
+    useEffect(() => {
+        setMounted(true);
+    }, []);
 
-  const {
-    register,
-    handleSubmit,
-    setError,
-    formState: { errors },
-  } = useForm<FormData>({
-    resolver: zodResolver(addFriendValidator),
-  });
+    const {
+        register,
+        handleSubmit,
+        setError,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: zodResolver(addFriendValidator),
+    });
 
-  const addFriend = async (email: string) => {
-    try {
-      const validatedEmail = addFriendValidator.parse({ email });
+    if (!mounted) return <>Loading...</>;
 
-      await axios.post("api/friends/add", {
-        email: validatedEmail,
-      });
+    const addFriend = async (email: string) => {
+        try {
+            const validatedEmail = addFriendValidator.parse({ email });
 
-      setShowSuccessState(true);
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        setError("email", { message: error.message });
-        return;
-      }
+            await axios.post('http://localhost:3000/api/friends/add', {
+                email: validatedEmail,
+            });
 
-      if (error instanceof AxiosError) {
-        setError("email", { message: error.response?.data });
-        return;
-      }
+            setShowSuccessState(true);
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                debugger;
+                setError('email', { message: error.message });
+                return;
+            }
 
-      setError("email", { message: "Something went wrong" });
-    }
-  };
+            if (error instanceof AxiosError) {
+                debugger;
+                setError('email', { message: error.response?.data });
+                // setError('email', { message: error.message });
+                return;
+            }
+            debugger;
+            setError('email', { message: 'Something went wrong' });
+        }
+    };
 
-  const onSubmit = (data: FormData) => {
-    addFriend(data.email);
-  };
+    const onSubmit = (data: FormData) => {
+        addFriend(data.email);
+    };
 
-  return (
-    <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm">
-      <label
-        htmlFor="email"
-        className="block text-sm font-medium leading-6 text-gray-900"
-      >
-        Add friend by E-Mail
-      </label>
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="max-w-sm">
+            <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+            >
+                Add friend by E-Mail
+            </label>
 
-      <div className="mt-2 flex gap-4">
-        <input
-          {...register("email")}
-          type="text"
-          className="block w-full rounded-md py-1.5 text-gray-900 shadow-md ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-          placeholder="you@example.com"
-        />
-        <Button>Add</Button>
-      </div>
-      <p className="mt-1 text-sm text-red-600">{errors.email?.message}</p>
-      {showSuccessState ? (
-        <p className="mt-1 text-sm text-green-600">Friend request sent!</p>
-      ) : null}
-    </form>
-  );
+            <div className="mt-2 flex gap-4">
+                <input
+                    className="block w-full rounded-md py-1.5 text-gray-900 shadow-md ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-400 sm:text-sm sm:leading-6"
+                    {...register('email')}
+                    type="email"
+                    placeholder="you@example.com"
+                />
+                <Button>Add</Button>
+            </div>
+            <p className="mt-1 text-sm text-red-600">{errors.email?.message}</p>
+            {showSuccessState ? (
+                <p className="mt-1 text-sm text-green-600">
+                    Friend request sent!
+                </p>
+            ) : null}
+        </form>
+    );
 };
 
 export default AddFriendButton;
