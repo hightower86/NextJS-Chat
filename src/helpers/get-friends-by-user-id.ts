c5683d3425a7b9092f090c3b2688c8ea3f88c3f4
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-08-28 13:26:37 +0400

    sidebar done

---
+++ src/helpers/get-friends-by-user-id.ts
@@ -0,0 +1,21 @@
+import { User } from 'lucide-react';
+import { fetchRedis } from './redis';
+export const getFriendsByUserId = async (userId: string) => {
+    // retrieve friends for current User
+    const friendIds = (await fetchRedis(
+        'smembers',
+        `user:${userId}:friends`
+    )) as string[];
+
+    const friends = await Promise.all(
+        friendIds.map(async (friendId) => {
+            const friend = (await fetchRedis(
+                'get',
+                `user:${friendId}`
+            )) as string;
+            const parsedFriend = JSON.parse(friend) as User;
+            return parsedFriend;
+        })
+    );
+    return friends;
+};
