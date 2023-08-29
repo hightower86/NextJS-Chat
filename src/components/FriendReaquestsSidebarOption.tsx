fdc517d30035c91699b8b98ae6124cb3ff233d85
arezvanov@arezvanov <adel.rezvanov@noveogroup.com>
2023-08-29 15:05:26 +0400

    Pusher. real-time add friend

--- src/components/FriendReaquestsSidebarOption.tsx
+++ src/components/FriendReaquestsSidebarOption.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import { pusherClient } from '@/lib/pusher';
+import { toPusherKey } from '@/lib/utils';
 import { User } from 'lucide-react';
 import Link from 'next/link';
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 interface FriendReaquestsSidebarOptionProps {
     initialUnseenRequestsCount?: number;
@@ -11,10 +13,34 @@ interface FriendReaquestsSidebarOptionProps {
 
 const FriendReaquestsSidebarOption: FC<FriendReaquestsSidebarOptionProps> = ({
     initialUnseenRequestsCount = 0,
+    sessionId,
 }) => {
     const [unseenRequestsCount, setUnseenRequestsCount] = useState<number>(
         initialUnseenRequestsCount
     );
+
+    useEffect(() => {
+        pusherClient.subscribe(
+            toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+        );
+
+        const friendRequestHandler = () => {
+            setUnseenRequestsCount((prev) => prev + 1);
+        };
+
+        pusherClient.bind('incoming_friend_requests', friendRequestHandler);
+
+        return () => {
+            pusherClient.unsubscribe(
+                toPusherKey(`user:${sessionId}:incoming_friend_requests`)
+            );
+            pusherClient.unbind(
+                'incoming_friend_requests',
+                friendRequestHandler
+            );
+        };
+    }, []);
+
     console.log({ unseenRequestsCount });
     return (
         <Link
